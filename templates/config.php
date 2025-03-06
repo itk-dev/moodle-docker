bb8be46a909cb96ab738d3d3ad860dbbc0c1e4f3
martinyde <yde001@gmail.com>
2025-03-06 07:50:12 +0100

    Updated setup

---
+++ templates/config.php
@@ -0,0 +1,39 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'mariadb';
+$CFG->dbname    = 'db';
+$CFG->dbuser    = 'db';
+$CFG->dbpass    = 'db';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_general_ci',
+);
+
+if (empty($_SERVER['HTTP_HOST'])) {
+  $_SERVER['HTTP_HOST'] = '127.0.0.1:8080';
+}
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+  $CFG->wwwroot   = 'https://' . 'moodle.local.itkdev.dk';
+} else {
+  $CFG->wwwroot   = 'http://' . 'moodle.local.itkdev.dk';
+}
+
+
+$CFG->dataroot  = '/app/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 02777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
